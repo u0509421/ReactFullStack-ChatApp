7d15ddf30f46b7bfe15f71fadace9548beff04f0
terrylyu <u0509421@gmail.com>
2025-01-10 15:13:46 +0800

    getMessage endpoint

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -27,13 +27,39 @@ export const sendMessage = async (req, res) => {
       conversation.messages.push(newMessage._id);
     }
 
-    // save conservation and new message to mongoDB
-    await conversation.save();
-    await newMessage.save();
+    // SOCKET IO FUNCTIONALITY WILL GO HERE
 
+    // save conservation and new message to mongoDB
+    // but this will run one after another and waste time
+    // await conversation.save();
+    // await newMessage.save();
+
+    // this will run in parallel
+    await Promise.all([conversation.save(), newMessage.save()]);
     res.status(201).json(newMessage);
   } catch (error) {
     console.log("error in sendMessage controller", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getMessage = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Consersation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages"); // not reference but actual messages
+
+    if (!conversation) {
+      return res.status(200).json([]);
+    }
+
+    const messages = conversation.messages;
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("error in getMessage controller", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
