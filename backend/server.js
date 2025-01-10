cc9358c2a3408fa55d5f9f06cba8ddd881290394
terrylyu <u0509421@gmail.com>
2025-01-10 16:00:22 +0800

    get users for sidebar

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-// file imports
+// routes imports
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -14,11 +16,13 @@ const PORT = process.env.PORT || 3000;
 dotenv.config();
 
 // middleware
+// cookieParser 要在express.json之前使用！！！！否则protectRoute那里无法拿到cookie
 app.use(cookieParser());
 app.use(express.json()); // for parsing application/json (from req.body)
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // app.get("/", (req, res) => {
 //   // root route http://localhost:3000
