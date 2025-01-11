28e2fc0a6d67b8522126b2ff0331f26c71bac1c6
terrylyu <u0509421@gmail.com>
2025-01-11 23:06:32 +0800

    authContext

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
-
+  const { setAuthUser } = useAuthContext();
   const signup = async ({
     fullName,
     username,
@@ -36,7 +37,16 @@ const useSignup = () => {
           gender,
         }),
       });
+
       const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      // local storage to check login status
+      localStorage.setItem("chat-user", JSON.stringify(data));
+      // context api
+      setAuthUser(data);
+
       console.log(data);
     } catch (error) {
       toast.error(error.message);
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDom from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
 
 ReactDom.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
